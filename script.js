498bb33c87207abe4f46a2b4e9f3bf011d52c0cf
Rafael Florentino <67845851+rafalosofo@users.noreply.github.com>
2023-07-05 11:56:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ var substantivos = ["flor", "amor", "mar", "lua", "poesia", "coração", "sol"];
 var adjetivos = ["belo", "brilhante", "encantador", "doce", "radiante", "sereno", "encantado"];
 var verbos = ["correr", "dançar", "cantar", "sonhar", "voar", "sorrir", "brilhar"];
 
-function gerarPrompt() {
-  var primeiraPalavra = getRandomWord(adjetivos);
-  var segundaPalavra = getRandomWord(substantivos);
-  var terceiraPalavra = getRandomWord(adjetivos);
-  var quartaPalavra = getRandomWord(verbos);
-  var quintaPalavra = getRandomWord(substantivos);
+var frase = "";
+var contadorPalavras = 0;
+
+function gerarPalavra() {
+  if (contadorPalavras === 0) {
+    frase = getRandomWord(adjetivos);
+  } else if (contadorPalavras === 1) {
+    frase += " " + getRandomWord(substantivos);
+  } else if (contadorPalavras === 2) {
+    frase += " " + getRandomWord(adjetivos);
+  } else if (contadorPalavras === 3) {
+    frase += " " + getRandomWord(verbos);
+  } else if (contadorPalavras === 4) {
+    frase += " " + getRandomWord(substantivos);
+  } else {
+    frase += " " + getRandomWord(substantivos) + "<br>";
+  }
   
-  var frase = primeiraPalavra + " " + segundaPalavra + " " + terceiraPalavra + " " + quartaPalavra + " " + quintaPalavra + ".";
+  document.getElementById("prompt").innerHTML = frase;
   
-  document.getElementById("prompt").innerText = frase;
+  contadorPalavras++;
+  
+  if (contadorPalavras === 6) {
+    contadorPalavras = 0;
+    frase = "";
+  }
 }
 
 function getRandomWord(wordArray) {
